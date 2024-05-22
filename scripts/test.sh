865aedee911c7573fe7af7dc5fc2daf0b9715b0e
Tobias Waslowski <tobias.waslowski@thoughtworks.com>
2024-05-22 16:19:36 +0200

    Measure test coverage, use dockerized OpenAI Mock server

---
+++ scripts/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+function cleanup {
+  echo "cleaning up"
+  docker stop openai-mock-server
+}
+trap 'cleanup' EXIT
+
+echo "launching openai mock server"
+docker run -d --rm \
+  --name openai-mock-server \
+  -p 5002:5002 \
+  -e MOCK_TYPE=echo \
+  tobiaswaslowski/openai-mock-server
+
+echo "running tests"
+if [ -d .venv ]; then
+  echo "found existing virtual environment. activating ..."
+  source .venv/bin/activate
+else
+  echo "no virtual environment found. assuming dependencies are available."
+fi
+
+poetry run coverage run -m pytest
+poetry run coverage html
+poetry run coverage xml -o test/coverage.xml
+poetry run coverage-badge -f -o test/coverage.svg
--- test/llm/openai/test_openai_adapter.py
+++ test/llm/openai/test_openai_adapter.py
@@ -2,16 +2,17 @@ import pytest
 from openai import OpenAI
 
 from openllm_messaging.llm.openai.openai_adapter import OpenAIAdapter
+from openllm_messaging.llm.model_message import Role
 
 
 @pytest.fixture
 def openai_adapter() -> OpenAIAdapter:
     adapter = OpenAIAdapter("some-token")
-    adapter.client = OpenAI(api_key="some-token", base_url="http://localhost:5002")
+    adapter.client = OpenAI(api_key="some-token", base_url="http://localhost:5002/v1/")
     return adapter
 
 
 def test_openai_adapter_chat_completion_simple(openai_adapter):
     response = openai_adapter.chat_completion_simple("Hello, world!")
     assert response.content == "Hello, world!"
-    assert response.role == "assistant"
+    assert response.role == Role.ASSISTANT
